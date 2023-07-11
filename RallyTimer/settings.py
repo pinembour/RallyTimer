d9169e2bcfc7b8b8485c20d23dc86003066b0af6
Pinembour <pinembour@teppelin.fr>
2023-07-11 10:34:06 +0200

    Change DB location for docker deployment

--- RallyTimer/settings.py
+++ RallyTimer/settings.py
@@ -82,7 +82,7 @@ ASGI_APPLICATION = 'RallyTimer.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
